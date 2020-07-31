0b19fd305afccddff24ed0f9eba1308277bd5da1
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2020-07-30 21:03:29 -0400

    fix: added cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 require('./config/database')
 
+
+var cors = require('cors')
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -22,6 +24,7 @@ app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
